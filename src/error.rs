12f808bdaa2c2a4903b82999625a5641b4c3bb3b
Silvan Schmidt <mail@silvanschmidt.ch>
2025-01-29 13:27:14 +0100

    feat: add /new route

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,8 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("failed to read the body of the request: {0}")]
+    BodyRead(std::io::Error),
+
     #[error("config error: {0}")]
     Settings(#[from] config::ConfigError),
     #[error("xdg error: {0}")]
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,39 @@
 mod error;
+use std::io::Read;
+
 pub use error::Error;
 
 mod settings;
+use rouille::Response;
 use settings::Settings;
 
 pub fn run() -> Result<(), Error> {
     let settings = Settings::new()?;
 
-    rouille::start_server(format!("0.0.0.0:{}", settings.port), move |_request| {
-        rouille::Response::text("Hello, World!")
-    });
+    rouille::start_server(
+        format!("0.0.0.0:{}", settings.port),
+        move |request| match request.url().as_str() {
+            "/new" => {
+                if request.method() != "POST" {
+                    return Response::text("this route only allows POST requests.")
+                        .with_status_code(405);
+                }
+
+                let mut body = request
+                    .data()
+                    .expect("should only panic if Request::data was called before.");
+                let mut note = String::new();
+                body.read_to_string(&mut note)
+                    .map_err(Error::BodyRead)
+                    .unwrap();
+
+                println!("{}", note);
+
+                Response::text("ok")
+            }
+            route => {
+                Response::text(format!("'{}' is not a valid route.", route)).with_status_code(404)
+            }
+        },
+    );
 }
