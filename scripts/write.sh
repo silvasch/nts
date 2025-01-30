9913bc6f3e640600eb0315b92fbc3a20201d0570
Silvan Schmidt <mail@silvanschmidt.ch>
2025-01-30 11:30:29 +0100

    feat: use EDITOR

--- scripts/write.sh
+++ scripts/write.sh
@@ -2,9 +2,13 @@
 
 HOST={{HOST}}
 
-read -p "> " NOTE < /dev/tty
-
 read -s -p "password: " PASSWORD < /dev/tty
 PASSWORD=$(echo $PASSWORD | base64)
+
+rm -f .nts-note
+touch .nts-note
+$EDITOR .nts-note
+NOTE=$(cat .nts-note)
+rm .nts-note
 
 curl -s -X POST -d "$NOTE" -H "Authorization: Basic $PASSWORD" $HOST/new
