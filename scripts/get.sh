ff1a428ad8cbe55d58cbfdb3d551cfbc6ce619ac
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-02 21:34:35 +0100

    feat: better text interface

--- scripts/get.sh
+++ scripts/get.sh
@@ -3,9 +3,11 @@
 read -s -p "pwd> " PASSWORD < /dev/tty
 HASH=$(echo $PASSWORD | tr -d \\n | tr -d " " | sha256sum | cut -d " " -f 1)
 AUTH=$(echo ":$HASH" | base64 -w 0)
+echo ""
 
 RESP=$(curl -s -H "Authorization: Basic $AUTH" localhost:9112/api/check-pwd)
 if [ "$RESP" != "ok" ]; then
+  echo "invalid password"
   exit 1
 fi
 
